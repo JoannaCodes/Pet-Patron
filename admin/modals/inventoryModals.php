7680443e7c6dad154d9d21dc27dcd4aa3e463787
Queennie Llido <72775101+qmsllido@users.noreply.github.com>
2022-02-23 19:16:31 +0800

    Add files via upload

--- admin/modals/inventoryModals.php
+++ admin/modals/inventoryModals.php
@@ -10,36 +10,40 @@
                         <div class="col-md-8 ps-md-0 pe-md-2">
                             <div class="mb-3">
                                 <label for="item-name" class="col-form-label">Name</label>
-                                <input type="text" class="form-control" id="item-name">
+                                <input type="text" class="form-control" name="product_name" >
                             </div>
                         </div>
                         <div class="col-md-2 ps-md-0 pe-md-0">
                             <div class="mb-3">
                                 <label for="item-stock" class="col-form-label">Stock</label>
-                                <input type="number" class="form-control" id="item-stock">
+                                <input type="number" class="form-control" name="product_stocks">
                             </div>
                         </div>
                         <div class="col-md-2 ps-md-2 pe-md-0">
                             <div class="mb-3">
                                 <label for="item-price" class="col-form-label">Price</label>
-                                <input type="number" class="form-control" id="item-price">
+                                <input type="number" class="form-control" name="product_price" >
                             </div>
                         </div>
                     </div>
                     <div class="mb-3">
                         <label for="item-image" class="form-label">Item Image</label>
-                        <input class="form-control" type="file" id="item-image" placeholder="Choose Image">
+                        <input class="form-control" type="file" name="product_image" placeholder="Choose Image">
                     </div>
                     <div class="mb-3">
                         <label for="desc-text" class="col-form-label">Description</label>
-                        <textarea class="form-control" id="desc-text"></textarea>
+                        <textarea class="form-control" name="product_description" ></textarea>
                     </div>
+                    <div class="modal-footer">
+                <button type="button" class="btn btn-primary" id="closeBtn">Cancel</button>
+                <button type="submit" class="btn btn-success" name="saveBtn" id="saveBtn">Save</button>
+            </div>
                 </form>
             </div>
-            <div class="modal-footer">
+            <!-- <div class="modal-footer">
                 <button type="button" class="btn btn-primary" id="closeBtn">Cancel</button>
-                <button type="button" class="btn btn-success" id="saveBtn">Save</button>
-            </div>
+                <button type="submit" class="btn btn-success" name="saveBtn" id="saveBtn">Save</button>
+            </div> -->
         </div>
     </div>
 </div>
@@ -56,36 +60,38 @@
                         <div class="col-md-8 ps-md-0 pe-md-2">
                             <div class="mb-3">
                                 <label for="item-name" class="col-form-label">Name</label>
-                                <input type="text" class="form-control" id="item-name">
+                                <input type="text" class="form-control" name="product_name" id="product_name">
                             </div>
                         </div>
                         <div class="col-md-2 ps-md-0 pe-md-0">
                             <div class="mb-3">
                                 <label for="item-stock" class="col-form-label">Stock</label>
-                                <input type="number" class="form-control" id="item-stock">
+                                <input type="number" class="form-control" name="product_stocks" id="product_stocks">
                             </div>
                         </div>
                         <div class="col-md-2 ps-md-2 pe-md-0">
                             <div class="mb-3">
                                 <label for="item-price" class="col-form-label">Price</label>
-                                <input type="number" class="form-control" id="item-price">
+                                <input type="number" class="form-control" name="product_price" id="product_price">
                             </div>
                         </div>
                     </div>
                     <div class="mb-3">
                         <label for="item-image" class="form-label">Item Image</label>
-                        <input class="form-control" type="file" id="item-image" placeholder="Choose Image">
+                        <input class="form-control" type="file" name="product_image" id="product_image" placeholder="Choose Image">
                     </div>
                     <div class="mb-3">
                         <label for="desc-text" class="col-form-label">Description</label>
-                        <textarea class="form-control" id="desc-text"></textarea>
+                        <textarea class="form-control" name="product_description" id="product_description"></textarea>
                     </div>
+                    <button type="button" class="btn btn-primary" id="closeBtn">Cancel</button>
+                    <button type="submit" class="btn btn-success" name="saveBtn" id="saveBtn">Save</button>
                 </form>
             </div>
-            <div class="modal-footer">
+            <!-- <div class="modal-footer">
                 <button type="button" class="btn btn-primary" id="closeBtn">Cancel</button>
-                <button type="button" class="btn btn-success" id="saveBtn">Save</button>
-            </div>
+                <button type="submit" class="btn btn-success" name="saveBtn" id="saveBtn">Save</button>
+            </div> -->
         </div>
     </div>
 </div>
