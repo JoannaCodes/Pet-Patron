eb56da4595c28b4413301a542f522e4851809818
Queennie Llido <72775101+qmsllido@users.noreply.github.com>
2022-02-23 19:16:07 +0800

    Add files via upload

--- admin/inventory.php
+++ admin/inventory.php
@@ -16,17 +16,30 @@
                 </tr>
             </thead>
             <tbody>
-                <tr>
-                    <th>0000</th>
-                    <td>0000</td>
-                    <td>0000</td>
-                    <td>0000</td>
-                    <td>0000</td>
-                    <td class"border-start">
-                        <button class="btn btn-warning mb-1" id="editBtn">Edit</button>
-                        <button class="btn btn-danger mb-1" id="deleteBtn">Delete</button>
-                    </td>
-                </tr>
+            <?php
+                    include "../admin/config/connection.php";
+
+                    $sql = "SELECT * FROM tbl_products";
+                    $product = mysqli_query($conn, $sql);
+
+                    foreach($product as $row){
+                        ?>
+                            <tr>
+                                <td><?php echo $row['product_id']; ?></td>
+                                <td><?php echo $row['product_name']; ?></td>
+                                <td><?php echo $row['product_description']; ?></td>
+                                <td><?php echo $row['product_price']; ?></td>
+                                <td><?php echo $row['product_stocks']; ?></td>
+                                <td>
+                                    <button class="btn btn-warning mb-1" id="editItemBtn">Edit Info</button>
+                                    <button class="btn btn-danger mb-1" id="deleteBtn">Delete</button>
+                                </td>
+                            </tr>
+                        <?php
+                    }
+
+                    mysqli_close($conn);
+                ?>
             </tbody>
         </table>
         </div>
@@ -38,6 +51,83 @@
     <?php include '../admin/modals/inventoryModals.php'; ?>
     <!-- Modal -->
 
+    <!-- Add Row -->
+    <script>
+        $(document).ready(function(){
+            $("#addBtn").click(function(){
+                $("#addItemModal").show();
+            });
+
+            $("#addItemModal #closeBtn").click(function(){
+                $("#addItemModal").hide();
+            });
+
+            $("#addItemModal #saveBtn").click(function(){
+                $("#addItemModal").hide();
+            });
+        });
+    </script>
+
+    <!-- Edit Row -->
+    <script>
+        $(document).ready(function(){
+            //Edit Row
+            $("#itemTable #editItemBtn").click(function(){
+                $("#editItemModal").show();
+
+                $tr = $(this).closest('tr');
+
+                var data = $tr.children("td").map(function () {
+                    return $(this).text();
+                }).get();
+
+                console.log(data);
+
+                
+                $('#product_id').val(data[0]);
+                $('#product_name').val(data[1]);
+                $('#product_description').val(data[2]);
+                $('#product_image').val(data[3]);
+                $('#product_price').val(data[4]);
+                $('#product_stocks').val(data[5]);
+
+            });
+
+            $("#editItemModal #closeBtn").click(function(){
+                $("#editItemModal").hide();
+            });
+
+            $("#editItemModal #editItemBtn").click(function(){
+                $("#editItemModal").hide();
+            });
+        });
+    </script>
+    <!-- Delete Row -->
+    <script>
+        $(document).ready(function(){
+            $("#itemTable #deleteBtn").click(function(){
+                $("#deleteModal").show();
+
+                $tr = $(this).closest('tr');
+
+                var data = $tr.children("td").map(function () {
+                    return $(this).text();
+                }).get();
+
+                console.log(data);
+
+                $('#delete_id').val(data[0]);
+            });
+
+            $("#deleteModal #closeBtn").click(function(){
+                $("#deleteModal").hide();
+            });
+
+            $("#deleteModal #deleteBtn").click(function(){
+                $("#deleteModal").hide();
+            });
+        });
+    </script>
     <script>
         $(document).ready(function(){
             $("#addBtn").click(function(){
